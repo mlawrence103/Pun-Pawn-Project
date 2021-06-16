3c6292c0e6e9671aa2fd388ba15397c1c9c9c189
Inna Lynn <innalynn@Innas-MacBook-Air.local>
2021-06-16 17:35:10 -0400

    updated through table model after code review

---
+++ server/db/models/lineItem.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const LineItem = db.define('lineItem', {
+    quantity: {
+        type: Sequelize.INTEGER,
+        validate:{
+            isNumeric: true,
+            isInt: true,
+            min: 1
+        }
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        validate:{
+            isNumeric: true,
+            isInt: true,
+            min: 1
+        }
+    }
+})
+
+module.exports = LineItem
