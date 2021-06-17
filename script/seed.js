68276a35346403d85f05ab9a7c4a254f370f039d
Mabel Lawrence <mabelmail3@gmail.com>
2021-06-17 16:03:50 -0400

    yay more puns in our inventory

--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,9 @@
-"use strict";
+'use strict';
 
-const { db } = require("../server/db");
-const {models: { Order, User, Pun }} = require("../server/db");
+const { db } = require('../server/db');
+const {
+  models: { Order, User, Pun },
+} = require('../server/db');
 
 /**
  * seed - this function clears the database, updates tables to
@@ -9,41 +11,73 @@ const {models: { Order, User, Pun }} = require("../server/db");
  */
 async function seed() {
   await db.sync({ force: true }); // clears db and matches models to tables
-  console.log("db synced!");
+  console.log('db synced!');
 
   const user1 = await User.create({
-    firstName: "Brenda",
-    lastName: "Wong",
-    email: "brenda.wong@gmail.com",
-    password: "123",
-    shippingAddressName: "Brenda Wong",
-    shippingAddressStreet: "123 Sesame St.",
-    shippingAddressCity: "New York City",
-    shippingAddressState: "NY",
-    shippingAddressZip: "10036",
-    billingAddressName: "Brenda Wong",
-    billingAddressStreet: "123 Sesame St.",
-    billingAddressCity: "New York City",
-    billingAddressState: "NY",
-    billingAddressZip: "10036",
-    userType: "ADMIN",
+    firstName: 'Brenda',
+    lastName: 'Wong',
+    email: 'brenda.wong@gmail.com',
+    password: '123',
+    shippingAddressName: 'Brenda Wong',
+    shippingAddressStreet: '123 Sesame St.',
+    shippingAddressCity: 'New York City',
+    shippingAddressState: 'NY',
+    shippingAddressZip: '10036',
+    billingAddressName: 'Brenda Wong',
+    billingAddressStreet: '123 Sesame St.',
+    billingAddressCity: 'New York City',
+    billingAddressState: 'NY',
+    billingAddressZip: '10036',
+    userType: 'ADMIN',
   });
 
   const pun1 = await Pun.create({
-    content: "pretty fly for a wifi",
-    author: "Ben Rodriguez",
-    price: 25.0,
+    content: 'pretty fly for a wifi',
+    author: 'Ben Rodriguez',
+    price: 25,
     quantity: 3,
   });
 
+  const pun2 = await Pun.create({
+    content:
+      "You're going to await awhile for the seed data as I've used all my seeds to plant trees.",
+    author: 'Ben Rodriguez',
+    price: 500,
+    quantity: 1,
+  });
+
+  const pun3 = await Pun.create({
+    content: "What's a computer's favorite beat? An algo-rhythm.",
+    price: 50,
+    quantity: 5,
+  });
+
+  const pun4 = await Pun.create({
+    content: "The past, the present and the future walk into a bar. It was tense.",
+    price: 5,
+    quantity: 50
+  }),
+
+  const pun5 = await Pun.create({
+    content: "To prove he was right, the flat-earther walked to the end of the Earth. He eventually came around.",
+    price: 15,
+    quantity: 20
+  })
+
+  const pun6 = await Pun.create ({
+    content: "What did the cell say when his sister cell stepped on his foot? Mitosis.",
+    price: 100,
+    quantity: 7
+  })
+
   const order1 = await Order.create({
-    status: "open",
-    emailAddress: "brenda.wong@gmail.com",
-    shippingAddressName: "Brenda Wong",
-    shippingAddressStreet: "123 Sesame St.",
-    shippingAddressCity: "New York City",
-    shippingAddressState: "NY",
-    shippingAddressZip: "10036",
+    status: 'open',
+    emailAddress: 'brenda.wong@gmail.com',
+    shippingAddressName: 'Brenda Wong',
+    shippingAddressStreet: '123 Sesame St.',
+    shippingAddressCity: 'New York City',
+    shippingAddressState: 'NY',
+    shippingAddressZip: '10036',
   });
 
   await order1.addPun(pun1);
@@ -55,16 +89,16 @@ async function seed() {
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log("seeding...");
+  console.log('seeding...');
   try {
     await seed();
   } catch (err) {
     console.error(err);
     process.exitCode = 1;
   } finally {
-    console.log("closing db connection");
+    console.log('closing db connection');
     await db.close();
-    console.log("db connection closed");
+    console.log('db connection closed');
   }
 }
 
