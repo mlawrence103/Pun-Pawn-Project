61a826a5e480ed24f2c26f93521a6584e2db45dd
Inna Lynn <innalynn@Innas-MacBook-Air.local>
2021-06-17 12:49:36 -0400

    single user and all users reducer created

---
+++ client/store/singleUser.js
@@ -0,0 +1,59 @@
+import axios from 'axios'
+import { addedUser } from './allUsers'
+
+//ACTION TYPES
+
+const SET_USER = 'SET_USER'
+
+//ACTION CREATOR
+
+export const setUser = (user) => {
+  return {
+    type: SET_USER,
+    user
+  }
+}
+
+//THUNK CREATORS
+
+export const fetchUser = (id) => {
+  return async (dispatch) => {
+    try {
+      const {data} = await axios.get(`/api/${id}`)
+      dispatch(setUser(data))
+    }
+    catch (error){
+      console.error(error)
+    }
+  }
+}
+
+export const singingUp = (newUser, history) => {
+  return async (dispatch) => {
+    try {
+      const {data} = await axios.post('/api/users', newUser)
+      dispatch(setUser(data))
+      history.push('/users/')
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+export const updatingAcount = (id, user) => {
+  return async (dispatch) => {
+    const {data} = await axios.put(`/api/users/${id}`, user)
+    dispatch(setUser(data))
+  }
+}
+
+// USER SUB_REDUCER
+export default function singleUserReducer(user = {}, action) {
+  switch (action.type) {
+    case SET_USER:
+      return action.user
+    default:
+      return users
+  }
+}
