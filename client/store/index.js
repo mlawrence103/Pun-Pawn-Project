8c5ddc4c8aff909e8aaa6227ca70b413496ae922
Inna Lynn <innalynn@Innas-MacBook-Air.local>
2021-06-18 10:24:52 -0400

    combined redux reducers

--- client/store/index.js
+++ client/store/index.js
@@ -2,9 +2,21 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
+import punsReducer from './allPuns'
+import allUsersReducer from './allUsers'
+import orderReducer from './order'
+import singlePunReducer from './singlePun'
+import singleUserReducer from './singleUser'
+
 import auth from './auth'
 
-const reducer = combineReducers({ auth })
+const reducer = combineReducers({ 
+  auth, 
+  allPuns: punsReducer, 
+  singlePun: singlePunReducer,
+  singleUser: singleUserReducer,
+  allUsers: allUsersReducer, 
+  orders: orderReducer,  })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
