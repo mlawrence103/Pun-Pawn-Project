5184bee7a491f1dae34cadf813a3e3fe93fb2a92
Mabel Lawrence <mabelmail3@gmail.com>
2021-06-22 15:33:47 -0400

    guest cart works now

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,11 +1,11 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const {
   models: { Order, Pun, LineItem, User },
-} = require("../db");
+} = require('../db');
 
 //get order by orderId (useful for guests)
 //security: if order has userId, then to access must be associated user or admin
-router.get("/:orderId", async (req, res, next) => {
+router.get('/:orderId', async (req, res, next) => {
   try {
     const orderId = req.params.orderId;
     //add eager loading to include where items' order id matches
@@ -22,6 +22,7 @@ router.get("/:orderId", async (req, res, next) => {
         },
       ],
     });
+    console.log('!!!!!!******order in get by orderId route: ', order);
     res.json(order);
   } catch (error) {
     next(error);
@@ -29,8 +30,8 @@ router.get("/:orderId", async (req, res, next) => {
 });
 
 //create new order
-router.post("/", async (req, res, next) => {
-  console.log(">>>>>>>>>>>>>>>>>>>>HERE IN NEW ORDER POST ROUTE");
+router.post('/', async (req, res, next) => {
+  console.log('>>>>>>>>>>>>>>>>>>>>HERE IN NEW ORDER POST ROUTE');
   try {
     //make sure getting proper order instance from store
     const {
@@ -51,7 +52,7 @@ router.post("/", async (req, res, next) => {
           {
             model: Order,
             where: {
-              status: "open",
+              status: 'open',
             },
           },
         ],
@@ -59,10 +60,10 @@ router.post("/", async (req, res, next) => {
       //if user already has an open order, do not create new order
       if (userWithOpenOrder) {
         //is this the proper way to reference this?
-        throw new Error("You cannot create more than one open order per user.");
+        throw new Error('You cannot create more than one open order per user.');
       }
       order = await Order.create({
-        status: "open",
+        status: 'open',
         emailAddress,
         shippingAddressName,
         shippingAddressStreet,
@@ -74,7 +75,7 @@ router.post("/", async (req, res, next) => {
     } else {
       //if guest cart, create new order without userId
       order = await Order.create({
-        status: "open",
+        status: 'open',
         emailAddress,
         shippingAddressName,
         shippingAddressStreet,
@@ -91,8 +92,8 @@ router.post("/", async (req, res, next) => {
 
 //**OPEN ORDER ROUTES */
 
-router.post("/addToCart", async (req, res, next) => {
-  console.log("reached add to cart API route");
+router.post('/addToCart', async (req, res, next) => {
+  console.log('reached add to cart API route');
   try {
     const { punId, orderId, qty, price } = req.body;
     const lineItem = await LineItem.create({
@@ -107,7 +108,7 @@ router.post("/addToCart", async (req, res, next) => {
   }
 });
 
-router.delete("/deleteItem", async (req, res, next) => {
+router.delete('/deleteItem', async (req, res, next) => {
   try {
     const item = await LineItem.findOne({
       where: {
@@ -122,7 +123,7 @@ router.delete("/deleteItem", async (req, res, next) => {
   }
 });
 
-router.put("/editLineItem", async (req, res, next) => {
+router.put('/editLineItem', async (req, res, next) => {
   try {
     const { punId, orderId, quantity } = req.body;
     const item = await LineItem.findOne({
@@ -140,7 +141,7 @@ router.put("/editLineItem", async (req, res, next) => {
   }
 });
 
-router.put("/:orderId/checkout", async (req, res, next) => {
+router.put('/:orderId/checkout', async (req, res, next) => {
   try {
     const {
       emailAddress,
@@ -165,11 +166,11 @@ router.put("/:orderId/checkout", async (req, res, next) => {
   }
 });
 
-router.put("/:orderId/submit", async (req, res, next) => {
+router.put('/:orderId/submit', async (req, res, next) => {
   try {
     const order = await Order.findByPk(req.params.orderId);
     await order.update({
-      status: "fulfilled",
+      status: 'fulfilled',
     });
     res.json(order);
   } catch (error) {
