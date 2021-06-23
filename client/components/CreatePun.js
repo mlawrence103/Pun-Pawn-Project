70292c42ba3855dd88971788e5d7981c63f897c2
Mabel Lawrence <mabelmail3@gmail.com>
2021-06-23 10:35:21 -0400

    create pun form works

--- client/components/CreatePun.js
+++ client/components/CreatePun.js
@@ -19,13 +19,14 @@ class CreatePun extends React.Component {
     this.setState({
       [event.target.name]: event.target.value,
     });
+    console.log(this.state);
   }
 
   handleSubmit(event) {
     event.preventDefault();
+    this.props.createPun({ ...this.state });
   }
   render() {
-    console.log('HERE in CREATE PUN');
     const { handleChange, handleSubmit } = this;
     const { content, author, price, quantity } = this.state;
     return (
@@ -33,49 +34,50 @@ class CreatePun extends React.Component {
         <form onSubmit={handleSubmit} id="create-pun-form">
           <h3>Add New Pun to Inventory</h3>
           <div id="pun-content">
-            <label htmlFor="pun-content">
+            <label htmlFor="content">
               <small>Content</small>
             </label>
             <input
-              name="punContent"
+              name="content"
               type="text"
               onChange={handleChange}
               value={content}
             />
           </div>
           <div id="pun-author">
-            <label htmlFor="pun-author">
+            <label htmlFor="author">
               <small>Author</small>
             </label>
             <input
-              name="punAuthor"
+              name="author"
               type="text"
               onChange={handleChange}
               value={author}
             />
           </div>
           <div id="pun-price">
-            <label htmlFor="pun-price">
+            <label htmlFor="price">
               <small>Price (in cents)</small>
             </label>
             <input
-              name="punPrice"
+              name="price"
               type="text"
               onChange={handleChange}
               value={price}
             />
           </div>
           <div id="pun-quantity">
-            <label htmlFor="pun-content">
+            <label htmlFor="quantity">
               <small>Quantity Available</small>
             </label>
             <input
-              name="punQuantity"
+              name="quantity"
               type="text"
               onChange={handleChange}
               value={quantity}
             />
           </div>
+          <button type="submit">Submit</button>
         </form>
       </div>
     );
