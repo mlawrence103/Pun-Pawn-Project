1df8c0d6513efb2946a9578d04cebfbc7a16bd6a
Brenda Wong <Brendot.wong@gmail.com>
2021-06-24 08:53:40 -0400

    changed default qty of line item in model

--- server/db/models/lineItem.js
+++ server/db/models/lineItem.js
@@ -1,23 +1,24 @@
-const Sequelize = require('sequelize')
-const db = require('../db')
+const Sequelize = require("sequelize");
+const db = require("../db");
 
-const LineItem = db.define('lineItem', {
-    quantity: {
-        type: Sequelize.INTEGER,
-        validate:{
-            isNumeric: true,
-            isInt: true,
-            min: 1
-        }
+const LineItem = db.define("lineItem", {
+  quantity: {
+    type: Sequelize.INTEGER,
+    validate: {
+      isNumeric: true,
+      isInt: true,
+      min: 1,
     },
-    price: {
-        type: Sequelize.INTEGER,
-        validate:{
-            isNumeric: true,
-            isInt: true,
-            min: 1
-        }
-    }
-})
+    defaultValue: 1,
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    validate: {
+      isNumeric: true,
+      isInt: true,
+      min: 1,
+    },
+  },
+});
 
-module.exports = LineItem
+module.exports = LineItem;
